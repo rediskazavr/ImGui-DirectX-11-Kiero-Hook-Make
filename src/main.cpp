0338d174c4d0b521eb4809e5d58b8db9f19a7735
MatveySDK <matveywindows60@gmail.com>
2025-04-24 14:23:45 +0300

    Init

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,92 @@ int getProc(HANDLE*, DWORD);
 
 int getDLLpath(char* dll){
     std::cout << "Enter path to dll\n";
+    cin >> dll;
+    return 1;
+
+}
+
+int getPID(int* PID){
+    cout << "Enter path PID to your target process\n";
+    cin >> *PID;
+    return 1;
+}
+
+int getProc(HANDLE* handleToProc, DWORD pid){
+    *handleToProc = OpenProcess(PROCESS_ALL_ACCESS, false, pid);
+    DWORD dwLastError = GetLastError();
+
+    if(*handleToProc = NULL){
+        std::cout << "Undable to open process\n";
+        return -1;
+    }
+    else{
+        std::cout << "Process Opened\n";
+        return 1;
+    }
+}
+
+int InjectDLL(DWORD PID, char* dll){
+    HANDLE handleToProc;
+    LPVOID LoadLibAddr;
+    LPVOID baseAddr;
+    HANDLE remThread;
+
+    // GET DLL LENGHT
+    int dllLenght = strlen(dll) + 1;
+
+    // HOOK PROCESSING PROCESS
+    if(getProc(&handleToProc, PID)< 0)
+        return -1;
+
+    // LOAD KERNEL32 LIB
+    LoadLibAddr = ((LPVOID)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibAddr"));
+
+    if(!LoadLibAddr)
+        return -1;
+
+    baseAddr = VirtualAllocEx(handleToProc, NULL, dllLenght, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+    if(!baseAddr)
+        return -1;
+
+    // WRITE PATH TO DLL
+    if(!WriteProcessMemory(handleToProc, baseAddr, dll, dllLenght, NULL))
+        return -1;
+
+    // CREATE REMOTE THREAD
+    remThread = CreateRemoteThread(handleToProc, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibAddr, baseAddr, 0, NULL);
+
+    if(!remThread)
+        return -1;
+
+    WaitForSingleObject(remThread, INFINITE);
+
+    VirtualFreeEx(handleToProc, baseAddr, dllLenght, MEM_RELEASE);
+
+    // CLOSE HANDLER
+    if(CloseHandle(remThread) == 0){
+        std::cout << "Failed to close handle to remote thread";
+        return -1;
+    }
+
+    if(CloseHandle(handleToProc) == 0)
+    {
+        std::cout << "Failed to close handle to target process\n";
+    }
 }
 
 int main(){
-    system("Injector");
+    SetConsoleTitle("Injector");
 
     int PID = -1;
     char *dll = new char[255];
+
+    getDLLpath(dll);
+    getPID(&PID);
+
+    InjectDLL(PID, dll);
+    system("pause");
+
+    return 0;
 }
